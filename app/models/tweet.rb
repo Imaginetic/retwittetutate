0ce2f26ba772e67672f1f88e6c2da270c91053fd
Imaginetic <imaginetic.studios@gmail.com>
2020-08-23 23:43:43 -0400

    Hito 1 - H2 H3 H4

---
+++ app/models/tweet.rb
@@ -0,0 +1,9 @@
+class Tweet < ApplicationRecord
+  validates :content, presence: true
+
+  belongs_to :user
+  has_many :likes
+  has_many :liking_users, :through => :likes, :source => :user
+
+  paginates_per 50
+end

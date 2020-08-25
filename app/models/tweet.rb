355c4a757b38a9f384b0594799ae7fc6b5d249b0
Imaginetic <imaginetic.studios@gmail.com>
2020-08-25 04:18:17 -0400

    last

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,5 +5,41 @@ class Tweet < ApplicationRecord
   has_many :likes
   has_many :liking_users, :through => :likes, :source => :user
 
-  paginates_per 50
+  paginates_per 10
+
+  def is_liked?(user)
+    if self.liking_users.include?(user)
+      true
+    else
+      false
+    end
+  end
+
+  def remove_like(user)
+    Like.where(user: user, tweet: self).first.destroy
+  end
+
+  def add_like(user)
+    Like.create(user: user, tweet: self)
+  end
+
+  def count_rt
+    Tweet.where(rt_ref: self.id).count
+  end
+
+  def is_retweet?
+    rt_ref ? true : false
+  end
+
+  def tweet_ref
+    Tweet.find(self.rt_ref)
+  end
+
+  def retweet_icon
+    if self.count_rt >0
+      "text-primary"
+    else
+      "text-muted"
+    end
+  end
 end

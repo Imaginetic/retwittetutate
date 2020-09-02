914498496e3abd739d6964ef7225f4dcb7128e9c
Imaginetic <imaginetic.studios@gmail.com>
2020-09-02 13:58:16 -0400

    hito II

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets
+  has_many :friends
   has_many :likes
   has_many :liked_tweets, :through => :likes, :source => :tweet
+# def arr_friends_id
+#  self.friends.pluck(:friend_id)
+# end
+
+  def users_followed
+    arr_ids= self.friends.pluck(:friend_id)
+    User.find(arr_ids)
+  end
+
+  def is_following?(user)
+    users_followed.include? (user)
+  end
 end
